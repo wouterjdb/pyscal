Fix endpoint handling in normalization code
Bugs found by hypothesis testing

tests/test_utils.py
@@ -8,6 +8,10 @@ from __future__ import print_function
 import pandas as pd
 import numpy as np
 
+from hypothesis import given, settings
+import hypothesis.strategies as st
+
+
 from pyscal import utils, WaterOil
 from test_wateroil import check_table
 
@@ -58,8 +62,65 @@ def test_diffjumppoint():
     assert utils.estimate_diffjumppoint(df, side="right") == 0.9
 
 
+@settings(deadline=1000)
+@given(
+    st.floats(min_value=0, max_value=0.1),  # swl
+    st.floats(min_value=0, max_value=0.0),  # dswcr
+    st.floats(min_value=0, max_value=0.1),  # dswlhigh
+    st.floats(min_value=0, max_value=0.3),  # sorw
+    st.floats(min_value=0, max_value=0.1),  # dsorw
+    st.floats(min_value=0.1, max_value=5),  # nw1
+    st.floats(min_value=0.01, max_value=1),  # krwend1
+    st.floats(min_value=0.1, max_value=5),  # now1
+    st.floats(min_value=0.01, max_value=1),  # kroend1
+    st.floats(min_value=0.1, max_value=5),  # nw2
+    st.floats(min_value=0.01, max_value=1),  # krwend2
+    st.floats(min_value=0.1, max_value=5),  # now2
+    st.floats(min_value=0.01, max_value=1),  # kroend2
+)
+def test_normalize_nonlinpart_hypo(
+    swl,
+    dswcr,
+    dswlhigh,
+    sorw,
+    dsorw,
+    nw1,
+    krwend1,
+    now1,
+    kroend1,
+    nw2,
+    krwend2,
+    now2,
+    kroend2,
+):
+    """Test the normalization code in utils.
+
+    In particular the fill_value argument to scipy has been tuned to
+    fulfill this code"""
+    ow_low = WaterOil(swl=swl, swcr=swl + dswcr, sorw=sorw)
+    ow_high = WaterOil(
+        swl=swl + dswlhigh, swcr=swl + dswlhigh + dswcr, sorw=max(sorw - 0.01, 0)
+    )
+    ow_low.add_corey_water(nw=nw1, krwend=krwend1)
+    ow_high.add_corey_water(nw=nw2, krwend=krwend2)
+    ow_low.add_corey_oil(now=now1, kroend=kroend1)
+    ow_high.add_corey_oil(now=now2, kroend=kroend2)
+
+    krwn1, kron1 = utils.normalize_nonlinpart(ow_low)
+    assert np.isclose(krwn1(0), 0)
+    assert np.isclose(krwn1(1), krwend1)
+    assert np.isclose(kron1(0), 0)
+    assert np.isclose(kron1(1), kroend1)
+
+    krwn2, kron2 = utils.normalize_nonlinpart(ow_high)
+    assert np.isclose(krwn2(0), 0)
+    assert np.isclose(krwn2(1), krwend2)
+    assert np.isclose(kron2(0), 0)
+    assert np.isclose(kron2(1), kroend2)
+
+
 def test_normalize_nonlinpart():
-    # Test that we can make normalized functions
+    """Manual tests for utils.normalize_nonlinpart"""
     wo = WaterOil(swl=0.1, swcr=0.12, sorw=0.05, h=0.05)
     wo.add_corey_water(nw=2.1, krwend=0.9)
     wo.add_corey_oil(now=3, kroend=0.8)
@@ -120,14 +181,41 @@ def test_normalize_nonlinpart():
     assert np.isclose(kron(1), 0.8)
 
 
-def test_interpolate_wo():
-    ow_low = WaterOil(swl=0.03, swcr=0.09, sorw=0.1)
-    ow_high = WaterOil(swl=0.1, swcr=0.13, sorw=0.05)
+@settings(deadline=1000)
+@given(
+    st.floats(min_value=0, max_value=0.1),  # swl
+    st.floats(min_value=0, max_value=0.0),  # dswcr
+    st.floats(min_value=0, max_value=0.1),  # dswlhigh
+    st.floats(min_value=0, max_value=0.3),  # sorw
+    st.floats(min_value=0, max_value=0.1),  # dsorw
+    st.floats(min_value=0, max_value=0.1),  # nw
+    st.floats(min_value=0, max_value=0.1),  # krwend
+)
+def test_interpolate_wo(swl, dswcr, dswlhigh, sorw, dsorw, nw, krwend):
+    ow_low = WaterOil(swl=swl, swcr=swl + dswcr, sorw=sorw)
+    ow_high = WaterOil(
+        swl=swl + dswlhigh, swcr=swl + dswlhigh + dswcr, sorw=max(sorw - 0.01, 0)
+    )
     ow_low.add_corey_water(nw=1, krwend=0.8)
     ow_high.add_corey_water(nw=2, krwend=0.7)
     ow_low.add_corey_oil(now=3, kroend=0.6)
     ow_high.add_corey_oil(now=2, kroend=0.95)
+    # print(
+    #     " ** Low curve (red):\n"
+    #     + ow_low.swcomment
+    #     + ow_low.krwcomment
+    #     + ow_low.krowcomment
+    # )
+    # print(
+    #     " ** High curve (red):\n"
+    #     + ow_high.swcomment
+    #     + ow_high.krwcomment
+    #     + ow_high.krowcomment
+    # )
 
     for t in np.arange(0, 1, 0.1):
         ow_ip = utils.interpolate_ow(ow_low, ow_high, t)
+        # print("Interpolation parameter: " + str(t))
+        # print(ow_ip.table)
+        # ow_ip.plotkrwkrow()
         check_table(ow_ip.table)